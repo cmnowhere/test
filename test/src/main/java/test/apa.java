9020df18253517b0ccf19cfcda722d1d95df5561
cmnowhere <cmnowhere@House>
2022-03-22 23:37:28 +0900

    bb

--- test/src/main/java/test/apa.java
+++ test/src/main/java/test/apa.java
@@ -16,9 +16,6 @@ public class apa {
 		System.out.println(dateFormat);
 		System.out.println(dateFormat);
 		System.out.println(dateFormat);
-		System.out.println(dateFormat);
-		System.out.println(dateFormat);
-		System.out.println(dateFormat);
 	}
 
 }
